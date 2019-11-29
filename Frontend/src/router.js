e0a7000707da92f1668193612237da196449c40b
CodeSquadEngUSJP <codesquadsjp@gmail.com>
2019-11-29 20:03:01 +0530

    Student login added

---
+++ Frontend/src/router.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import DashboardLayout from '@/layout/DashboardLayout'
+import AuthLayout from '@/layout/AuthLayout'
+Vue.use(Router)
+
+export default new Router({
+  linkExactActiveClass: 'active',
+  mode:'history',
+  routes: [
+    
+    {
+      path: '/',
+      redirect: 'login',
+      component: AuthLayout,
+      children: [
+        {
+          path: '/login',
+          name: 'login',
+          component: () => import(/* webpackChunkName: "demo" */ './views/Login.vue')
+        },
+        {
+          path: '/register',
+          name: 'register',
+          component: () => import(/* webpackChunkName: "demo" */ './views/Register.vue')
+        }
+      ]
+    }
+  ]
+})
